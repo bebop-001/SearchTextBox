9e52744bbba86b3f477585c3c7d0289260188de6
sjs <sjs@sonic.net>
2015-03-17 18:38:45 -0700

    Added an action bar spinner to allow user to select theme from Holo Dark or Holo Light.

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
+Mon Mar 16 13:52:26 PDT 2015
 
 I needed a couple of search-boxs that could be cleared and would
 use carriage return or IME Next to indicate selection was complete.
@@ -10,5 +11,8 @@ initializes them.
 Please feel free to use as you wish.  Attribution is appreciated though.
 
 Steven Smith
-Mon Mar 16 13:52:26 PDT 2015
+
+Tue Mar 17 18:36:20 PDT 2015
+Added a spinner that is used to select the app theme from 
+Hollo Light or Hollo Dark.
 
--- src/com/example/searchtextbox/MainActivity.java
+++ src/com/example/searchtextbox/MainActivity.java
@@ -1,19 +1,77 @@
 package com.example.searchtextbox;
 
+import java.util.ArrayList;
+
+import android.app.ActionBar;
+import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.searchtextbox.SearchTextBox.OnSearchReady;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnNavigationListener {
 	private static final String logTag = "MainActivity";
+    private static ArAd abAdapt;
+    private static SharedPreferences prefs;
+
+	private class ArAd extends ArrayAdapter<ThemeInfo> {
+		private boolean viewed = false;
+	    private ArAd(Activity a
+	            , int layoutId, int resId, ArrayList<ThemeInfo> themeinfo) {
+	        super(a, layoutId, resId, themeinfo);
+	    }
+	    @Override
+	    public View getDropDownView(int position, View convertView,
+	            ViewGroup parent) {
+	        viewed = true;
+	        return super.getDropDownView(position, convertView, parent);
+	    }
+	    private boolean viewed() {return viewed; }
+	}
+	private static class ThemeInfo {
+		private String name; private int resId;
+		private ThemeInfo (String name, int resId) {
+			this.name = name; this.resId = resId;
+		}
+		public String toString() {return name; }
+	}
+	private static final ArrayList<ThemeInfo> themeInfo;
+	static {
+		themeInfo = new ArrayList<ThemeInfo>();
+		themeInfo.add(new ThemeInfo("Holo Dark", android.R.style.Theme_Holo));
+		themeInfo.add(new ThemeInfo("Holo Light", android.R.style.Theme_Holo_Light));
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// setTheme(android.R.style.Theme_Holo);
+    super.onCreate(savedInstanceState);
 
+        /****************** Theme setup ********************************/
+    	// Restore user's previous selected theme.
+    	prefs = getSharedPreferences("user_prefs.txt", Context.MODE_PRIVATE);
+    	int selectedTheme = prefs.getInt("selectedTheme", 0);
+
+        setTheme(themeInfo.get(selectedTheme).resId);
+
+        ActionBar ab = getActionBar();
+        ab.setDisplayShowTitleEnabled(false);
+        ab.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        ab.setListNavigationCallbacks(null, null);
+
+        abAdapt = new ArAd (this
+            , android.R.layout.simple_list_item_1, android.R.id.text1, themeInfo);
+        ab.setListNavigationCallbacks(abAdapt, MainActivity.this);
+        ab.setSelectedNavigationItem(selectedTheme);
+
+        /********************** search box stuff ***********************/
 		setContentView(R.layout.main);
 		SearchTextBox sb1 
 			= new SearchTextBox(this, R.id.first_searchbox)
@@ -26,9 +84,8 @@ public class MainActivity extends Activity {
 						Log.i(logTag, "first searchDone:" + v.getText());
 					}
 				});
-		String tag = (String)sb1.getTag();
-		Log.i(logTag, "sb1 tag:" + tag);
-		SearchTextBox sb12 
+		Log.i(logTag, "sb1 tag:" + sb1.getTag());
+		SearchTextBox sb2 
 			= new SearchTextBox(this, R.id.second_searchbox)
 				.setHint("second search")
 				.setMinWidth(250)
@@ -38,6 +95,20 @@ public class MainActivity extends Activity {
 						Log.i(logTag, "second searchDone:" + v.getText());
 					}
 				});
-		
+		Log.i(logTag, "sb2 tag:" + sb2.getTag());
+
+	}
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        if (abAdapt.viewed()) {
+        	// save the user's selection.
+        	Log.i(logTag, "onNavigationItemSelected:" + itemPosition);
+        	Editor e = prefs.edit();
+            e.putInt("selectedTheme", itemPosition);
+            e.commit();
+            finish();
+            startActivity(new Intent(this, this.getClass()));
+        }
+		return false;
 	}
 }
